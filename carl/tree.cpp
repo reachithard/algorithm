278d841b98764aff62d709309a18d81e34b235d6
lzerone <lzerone@163.com>
2022-08-03 23:26:18 +0800

    提交路径和

--- carl/tree.cpp
+++ carl/tree.cpp
@@ -268,6 +268,59 @@ struct TreeNode {
       tmp.pop_back();
     }
   }
+
+  void LayerdPush(const vector<int> &tmp) {
+    queue<TreeNode *> que;
+    que.push(this);
+    int idx = 0;
+    while (!que.empty() && idx < tmp.size()) {
+      TreeNode *top = que.front();
+      if (top->left == nullptr) {
+        TreeNode *tt = new TreeNode(tmp[idx++]);
+        top->left = tt;
+        que.push(tt);
+      } else if (top->right == nullptr) {
+        TreeNode *tt = new TreeNode(tmp[idx++]);
+        top->right = tt;
+        que.push(tt);
+      } else {
+        que.pop();
+      }
+    }
+  }
+
+  bool TargetPath(int target) {
+    vector<int> paths;
+    return TargetPath(this, paths, target);
+  }
+
+  bool TargetPath(TreeNode *root, vector<int> &paths, int target) {
+    paths.push_back(root->val);
+    if (root->left == nullptr && root->right == nullptr) {
+      int sum = 0;
+      for (int i = 0; i < paths.size(); i++) {
+        sum += paths[i];
+      }
+
+      if (sum == target) {
+        return true;
+      }
+    }
+
+    bool left = false;
+    if (root->left) {
+      left = TargetPath(root->left, paths, target);
+      paths.pop_back();
+    }
+
+    bool right = false;
+    if (root->right) {
+      right = TargetPath(root->right, paths, target);
+      paths.pop_back();
+    }
+
+    return left || right;
+  }
 };
 
 /*
@@ -517,4 +570,53 @@ TEST(tree, leetcode257) {
     }
     cout << endl;
   }
+}
+
+/*
+给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在
+根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum
+。如果存在，返回 true ；否则，返回 false 。
+
+叶子节点 是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+输出：true
+解释：等于目标和的根节点到叶节点路径如上图所示。
+示例 2：
+
+
+输入：root = [1,2,3], targetSum = 5
+输出：false
+解释：树中存在两条根节点到叶子节点的路径：
+(1 --> 2): 和为 3
+(1 --> 3): 和为 4
+不存在 sum = 5 的根节点到叶子节点的路径。
+示例 3：
+
+输入：root = [], targetSum = 0
+输出：false
+解释：由于树是空的，所以不存在根节点到叶子节点的路径。
+ 
+
+提示：
+
+树中节点的数目在范围 [0, 5000] 内
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+TEST(tree, leetcode112) {
+  TreeNode *root = new TreeNode(5);
+  vector<int> arr = {4, 8, 11, -1, 13, 4, 7, 2, -1, -1, -1, 1};
+  root->LayerdPush(arr);
+  root->Print();
+  ASSERT_TRUE(root->TargetPath(22));
 }
