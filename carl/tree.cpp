51bda8317bf366a00647823e4778f4b7b645801e
lzerone <lzerone@163.com>
2022-08-10 22:54:34 +0800

    提交修剪二叉搜索树到范围

--- carl/tree.cpp
+++ carl/tree.cpp
@@ -780,6 +780,35 @@ public:
 
   void DeleteBstTree(int val) { DeleteBstTree(dummy, val); }
 
+  void DeleteBstToRange(int min, int max) { DeleteBstToRange(dummy, min, max); }
+
+  TreeNode *DeleteBstToRange(TreeNode *root, int min, int max) {
+    if (root == nullptr) {
+      return root;
+    }
+
+    if (root->val < min) {
+      return root;
+    } else if (root->val > max) {
+      return root;
+    }
+    TreeNode *leftNode = DeleteBstToRange(root->left, min, max);
+
+    TreeNode *rightNode = DeleteBstToRange(root->right, min, max);
+
+    if (leftNode) {
+      root->left = leftNode->right;
+      // 内存回收
+    }
+
+    if (rightNode) {
+      root->right = rightNode->left;
+      // 内存回收
+    }
+
+    return nullptr;
+  }
+
 private:
   TreeNode *dummy;
 };
@@ -1580,6 +1609,57 @@ TEST(tree, leetcode450) {
   root->Print();
 }
 
+/*
+给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界
+high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树
+不应该 改变保留在树中的元素的相对结构
+(即，如果没有被移除，原有的父代子代关系都应当保留)。
+可以证明，存在 唯一的答案 。
+
+所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,0,2], low = 1, high = 2
+输出：[1,null,2]
+示例 2：
+
+
+输入：root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+输出：[3,2,null,1]
+ 
+
+提示：
+
+树中节点数在范围 [1, 104] 内
+0 <= Node.val <= 104
+树中每个节点的值都是 唯一 的
+题目数据保证输入是一棵有效的二叉搜索树
+0 <= low <= high <= 104
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/trim-a-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 TEST(tree, leetcode669) {
-  
+  Tree *root = new Tree();
+  vector<int> arr = {1, 0, 2};
+  root->LayerdPush(arr);
+  root->Print();
+  root->DeleteBstToRange(1, 2);
+
+  cout << "delete" << endl;
+  root->Print();
+
+  Tree *root2 = new Tree();
+  vector<int> arr2 = {3, 0, 4, -1, 2, 3, 5, -1, -1, 1};
+  root2->LayerdPush(arr2);
+  root2->Print();
+  root2->DeleteBstToRange(1, 3);
+
+  cout << "delete" << endl;
+  root2->Print();
 }
